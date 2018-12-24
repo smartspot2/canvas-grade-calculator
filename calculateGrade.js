28ee712462a8a00a783054b14a5c339885fcd4dc
smartspot2 <smartspot2@gmail.com>
2018-12-24 02:25:37 -0800

    Initial commit with TODO

---
+++ calculateGrade.js
@@ -0,0 +1,11 @@
+// Loop through all categories
+//      For each category, loop through all assignments
+//          Keep tally of total points possible
+//          Keep tally of total points gotten
+//      Multiply fraction by weight
+//      Set category score value to DOM element next to category name and percentage
+// Add all fractions together to get total grade
+
+function calculateGrade() {
+
+}
---
+++ updateAssignments.js
@@ -0,0 +1,4 @@
+// When user clicks away from a assignmentScoreGotten DOM element or assignmentScorePossible DOM element,
+// update assignment object and recalculate grade.
+
+// If updated assignment possible points is blank, then exclude that assignment from the calculation.
