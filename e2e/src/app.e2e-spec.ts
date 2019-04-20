a00d0150b25efdf2744273428691bc20327e60ae
smartspot2 <smartspot2@gmail.com>
2019-04-19 21:22:33 -0700

    Changed directory name references

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getTitleText()).toEqual('Welcome to Canvas-Grade-Calculator!');
+        expect(page.getTitleText()).toEqual('Welcome to canvas-grade-calculator!');
     });
 
     afterEach(async () => {
